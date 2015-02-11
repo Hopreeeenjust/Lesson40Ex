4a250d7901278da978aee619ef5a10d914c63b17
Roman Jouravkov <roma1990may@rambler.ru>
2015-02-11 20:15:55 +0300

    Superman task added

--- Lesson40Ex/AppDelegate.m
+++ Lesson40Ex/AppDelegate.m
@@ -11,33 +11,44 @@
 
 @interface AppDelegate ()
 @property (strong, nonatomic) RJStudent *student;
+@property (strong, nonatomic) NSArray *students;
 @end
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    RJStudent *student1 = [RJStudent new];
-    RJStudent *student2 = [RJStudent new];
-    RJStudent *student3 = [RJStudent new];
-    RJStudent *student4 = [RJStudent new];
-    NSArray *students = @[student1, student2, student3, student4];
+    NSArray *maleNamesArray = @[@"Roma", @"Bill", @"Sam", @"Denis", @"Alex", @"Sergey", @"Bob", @"Rob", @"John", @"Simon", @"Nick", @"Piter", @"James", @"Kiran", @"Martin", @"Igor", @"Maks", @"Robby", @"Harry", @"Vova", @"Dima"];
+    NSArray *femaleNamesArray = @[@"Alexandra", @"Anna", @"Liz", @"Helen", @"Katty", @"Veronica", @"Jane", @"Cristina", @"Britney", @"Darya", @"Naomy", @"Maria", @"Angelika", @"Svetlana", @"Natalia", @"Marina", @"Margaret"];
+    NSArray *surnamesArray = @[@"Gibbs", @"Smith", @"Beniakovski", @"Long", @"Potter", @"Bond", @"Russel", @"Ostin", @"Mendez", @"Green", @"Bullock", @"Jones", @"Freeman", @"Murphy", @"Hanks", @"Goldberg", @"Williams", @"Depp", @"Hoffman", @"Martin", @"Law", @"Oldman", @"Carter", @"Hopkins", @"Delon"];
     
-    [student4 addObserver:self forKeyPath:@"firstName" options: NSKeyValueObservingOptionNew |
-     NSKeyValueObservingOptionOld context:nil];
-
-    student1.bestFriend = student2;
-    student2.bestFriend = student3;
-    student3.bestFriend = student4;
-    student4.bestFriend = student1;
+    self.students = [NSArray new];
     
-    [student1 setValue:@"Alex" forKeyPath:@"bestFriend.firstName"];
-    [student1 setValue:@"Sam" forKeyPath:@"bestFriend.bestFriend.firstName"];
-    [student1 setValue:@"Nick" forKeyPath:@"bestFriend.bestFriend.bestFriend.firstName"];
-    [student1 setValue:@"John" forKeyPath:@"bestFriend.bestFriend.bestFriend.bestFriend.firstName"];
+    for (NSInteger i = 0; i < 15; i++) {
+        RJStudent *student = [RJStudent new];
+        student.gender = arc4random_uniform(2 * 1000) / 1000;
+        if (student.gender == RJStudentGenderTypeFemale) {
+            student.firstName = [femaleNamesArray objectAtIndex:(int)(arc4random_uniform((int)[femaleNamesArray count] * 1000) / 1000)];
+        } else {
+            student.firstName = [maleNamesArray objectAtIndex:(int)(arc4random_uniform((int)[maleNamesArray count] * 1000) / 1000)];
+        }
+        student.lastName = [surnamesArray objectAtIndex:(int)(arc4random_uniform((int)[surnamesArray count] * 1000) / 1000)];
+        NSInteger age = arc4random_uniform(18 * 31556926) + 17 * 31556926;
+        student.dateOfBirth = [NSDate dateWithTimeIntervalSinceNow:-age];
+        student.grade = arc4random_uniform(11 * 1000) / 1000;
+        [[self mutableArrayValueForKey:@"students"] addObject:student];
+    }
     
-    self.student = student4;
+    NSArray *namesArray = [self valueForKeyPath:@"students.@unionOfObjects.firstName"];
+    NSLog(@"%@", namesArray);
     
+    NSDate *firstDate = [self valueForKeyPath:@"students.@min.dateOfBirth"];
+    NSDate *lastDate = [self valueForKeyPath:@"students.@max.dateOfBirth"];
+    NSDateFormatter *formatter = [NSDateFormatter new];
+    [formatter setDateFormat:@"dd/MMM/yyyy"];
+    NSLog(@"The youngest student was born on %@ and the oldest one - on %@", [formatter stringFromDate:lastDate], [formatter stringFromDate:firstDate]);
+    
+    NSLog(@"Average grade is %@ and total grade is %@", [self valueForKeyPath:@"students.@avg.grade"], [self valueForKeyPath:@"students.@sum.grade"]);
     return YES;
 }
 
