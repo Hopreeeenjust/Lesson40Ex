b7f161023965134b2a4ffd3233847ad140312e47
Roman Jouravkov <roma1990may@rambler.ru>
2015-02-11 13:36:46 +0300

    Initial Commit

---
+++ Lesson40Ex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson40Ex
+//
+//  Created by Hopreeeeenjust on 11.02.15.
+//  Copyright (c) 2015 Hopreeeeenjust. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lesson40ExTests/Lesson40ExTests.m
@@ -0,0 +1,40 @@
+//
+//  Lesson40ExTests.m
+//  Lesson40ExTests
+//
+//  Created by Hopreeeeenjust on 11.02.15.
+//  Copyright (c) 2015 Hopreeeeenjust. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Lesson40ExTests : XCTestCase
+
+@end
+
+@implementation Lesson40ExTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
